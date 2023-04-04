beef84815a26e2018a7355c00fd37644290d6c2b
NgoThien2k <bethien1230@gmail.com>
2023-04-04 15:33:03 +0700

    thien4.4

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,13 @@
-// window.onscroll = function(){
-//     scrollFunction();
-// }
-// function scrollFunction(){
-//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//         document.getElementById("navbar").style.top = "0"; 
+$('navbar').scrollspy({ target: '#navbar' })
+window.onscroll = function(){
+    scrollFunction();
+}
+function scrollFunction(){
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("navbar").style.top = "0"; 
 
-//     }
-//     else{
-//         document.getElementById("navbar").style.top = "-50px";
-//     }
-// }
+    }
+    else{
+        document.getElementById("navbar").style.top = "-50px";
+    }
+}
